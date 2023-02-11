b33de231ec1a63ba3e6d3b69a2c8fd3731aea5c1
Joey Battistoni <56656056+joejoebat@users.noreply.github.com>
2023-02-10 19:51:21 -0800

    fieldAt functon for point charge

--- src/charges/point_charge.ts
+++ src/charges/point_charge.ts
@@ -1,13 +1,13 @@
 import Object from "../base";
 import Vector from "../vector";
+import constants from "../constants";
 
 export default class PointCharge extends Object {
     //Stored in microcoloumbs
     charge: number;
     displayRadius: number;
 
-    feildAt = (pos: Vector) => {
-        let nVec = pos.unit();
-        return 0;
+    fieldAt = (pos: Vector) => {
+        return  Vector.multiply(Vector.inverseSquareField(pos, this.position), constants.K * this.charge);
     }
 }
