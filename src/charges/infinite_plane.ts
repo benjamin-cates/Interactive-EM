bfa8d900ace4d5f95c94f4d6c60e3d88666f2859
Joey Battistoni <56656056+joejoebat@users.noreply.github.com>
2023-02-10 21:16:59 -0800

    Update infinite_plane.ts

--- src/charges/infinite_plane.ts
+++ src/charges/infinite_plane.ts
@@ -1,8 +1,13 @@
 import Object from "../base";
 import Vector from "../vector";
+import constants from "../constants";
 
 export default class InfinitePlane extends Object {
     //Measured in microcoulombs per meter
     chargeDensity: number;
+    normal: Vector;
 
+    fieldAt = (pos: Vector) => {
+        return  Vector.multiply(this.normal, Math.sign(Vector.dot(this.normal, pos)) * 2 * this.chargeDensity * constants.K);
+    }
 }
