f6ee1ba096062fbf2a00a39a27a825fe4c96ccfa
Joey Battistoni <56656056+joejoebat@users.noreply.github.com>
2023-02-17 16:31:26 -0800

    Update conductor.ts

--- src/charges/conductor.ts
+++ src/charges/conductor.ts
@@ -1,10 +1,7 @@
-import {Object, ObjectTypes} from "../base";
-import Vector from "../vector";
+import {Object} from "../base";
 
 export default class Conductor extends Object {
     width: number;
     height: number;
-    chargeState: number[];
-    getType: () => ObjectTypes = () => "conductor";
 
 }
