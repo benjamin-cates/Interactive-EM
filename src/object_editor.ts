ece210bf22b20c678b39ca774ca4936b7f6c08cc
Benjamin Cates <bjc2dogs@gmail.com>
2023-02-19 12:56:15 -0800

    Add logarithmic property correction

--- src/object_editor.ts
+++ src/object_editor.ts
@@ -9,15 +9,33 @@ import { Object, ObjectTypes } from "./base";
 import Scene from "./scene";
 
 
+interface Correction {
+    correct: (number) => number;
+    uncorrect: (number) => number;
+};
+
+const noCorrection = {
+    correct: (x) => x,
+    uncorrect: (x) => x,
+} as Correction;
+const logCorrection = {
+    correct: (x) => Math.exp(x),
+    uncorrect: (x) => Math.log(x),
+} as Correction;
+const powerCorrection = {
+    correct: (x) => Math.sign(x) * Math.pow(Math.abs(x), 2.6),
+    uncorrect: (x) => Math.sign(x) * Math.pow(Math.abs(x), 1 / 2.6),
+} as Correction;
+
 interface Slider {
     name: string;
     type: "vector" | "number" | "boolean";
     min?: number | Vector;
     max?: number | Vector;
-    nonLinear?: boolean;
     step?: number;
     for?: "all" | ObjectTypes[];
     unit?: string;
+    correction: Correction;
 }
 const canMove: ObjectTypes[] = ["point_charge", "finite_line", "solid_charge", "conductor"];
 const canRotate: ObjectTypes[] = ["finite_line", "infinite_plane", "solid_charge", "conductor"];
@@ -25,71 +43,79 @@ const sliders: Slider[] = [
 
     //Universal
     {
-        "name": "position",
-        "type": "vector", "unit": "m",
-        "min": new Vector(-20, -20), "max": new Vector(20, 20), "step": 0.1,
-        "for": "all",
+        name: "position",
+        type: "vector", unit: "m",
+        min: new Vector(-20, -20), max: new Vector(20, 20), step: 0.1,
+        for: "all",
+        correction: noCorrection,
     },
     {
-        "name": "velocity",
-        "type": "vector", "unit": "m/s",
-        "min": new Vector(-20, -20), "max": new Vector(20, 20), "step": 0.1,
-        "nonLinear": true,
-        "for": canMove,
+        name: "velocity",
+        type: "vector", unit: "m/s",
+        min: new Vector(-20, -20), max: new Vector(20, 20), step: 0.1,
+        for: canMove,
+        correction: powerCorrection,
     },
     {
-        "name": "rotation",
-        "type": "number", "unit": "rad",
-        "min": -Math.PI, "max": Math.PI, "step": 0.1,
-        "for": canRotate,
+        name: "rotation",
+        type: "number", unit: "rad",
+        min: -Math.PI, max: Math.PI, step: 0.1,
+        for: canRotate,
+        correction: noCorrection,
     },
     {
-        "name": "angular_velocity",
-        "type": "number", "unit": "rad/s",
-        "min": -1.7, "max": 1.7, "step": 0.1,
-        "for": canRotate,
+        name: "angular_velocity",
+        type: "number", unit: "rad/s",
+        min: -1.7, max: 1.7, step: 0.1,
+        for: canRotate,
+        correction: powerCorrection,
     },
     {
-        "name": "mass",
-        "type": "number", "unit": "kg",
-        "min": -10, "max": 10, "step": 0.1,
-        "for": canMove,
+        name: "mass",
+        type: "number", unit: "kg",
+        min: 0.5, max: 1000, step: 0.05,
+        for: canMove,
+        correction: logCorrection,
     },
 
     //Point charge
     {
-        "name": "charge",
-        "type": "number", "unit": "μC",
-        "min": -3, "max": 3, "step": 0.05,
-        "nonLinear": true,
-        "for": ["point_charge"],
+        name: "charge",
+        type: "number", unit: "μC",
+        min: -3, max: 3, step: 0.05,
+        for: ["point_charge"],
+        correction: powerCorrection,
     },
 
     //Finite line
     {
-        "name": "length",
-        "type": "number", "unit": "m",
-        "min": 0, "max": 10, "step": 0.1,
-        "for": ["finite_line"],
+        name: "length",
+        type: "number", unit: "m",
+        min: 0.4, max: 20, step: 0.05,
+        for: ["finite_line"],
+        correction: logCorrection,
     },
     {
-        "name": "charge_density",
-        "type": "number", "unit": "μC/m",
-        "min": -1.5, "max": 1.5, "step": 0.05,
-        "nonLinear": true,
-        "for": ["finite_line"],
+        name: "charge_density",
+        type: "number", unit: "μC/m",
+        min: -1.5, max: 1.5, step: 0.05,
+        for: ["finite_line"],
+        correction: powerCorrection,
     },
 
     //Infinite Plane
     {
-        "name": "charge_density",
-        "type": "number", "unit": "nC/m²",
-        "min": -40, "max": 40, "step": 0.05,
-        "nonLinear": true,
-        "for": ["infinite_plane"],
+        name: "charge_density",
+        type: "number", unit: "nC/m²",
+        min: -40, max: 40, step: 0.05,
+        for: ["infinite_plane"],
+        correction: powerCorrection,
     },
 
 ];
+function getSliderId(name: string, type: ObjectTypes) {
+    return sliders.findIndex((slider) => (slider.name == name && (slider.for == "all" || slider.for.includes(type))));
+}
 export default class ObjEditor {
     static correction(value: number, isNonLinear: boolean = false): number {
         if (isNonLinear) return Math.sign(value) * Math.pow(Math.abs(value), 2.6);
@@ -114,23 +140,23 @@ export default class ObjEditor {
         let elements: Element[] = [];
         //For each element in curState
         for (let i in this.curState) {
-            let id = sliders.findIndex((slider) => (slider.name == i && (slider.for == "all" || slider.for.includes(this.curType))));
+            let id = getSliderId(i,this.curType);
             let name = sliders[id].name.charAt(0).toUpperCase() + sliders[id].name.slice(1).replace(/_/g, " ");
             let html = `<div class="input_slider slider_${i}">`;
             html += `<div class="input_slider_name">${name}</div>`;
             html += `<div class="input_slider_display">
                 <span class="slider_value" id="slider_value${i}">${this.curState[i].toString()}</span>
                 <span class="slider_units">${sliders[id].unit}</span></div>`;
-            let isNonLinear = sliders[id].nonLinear;
+            let uncorrect = sliders[id].correction.uncorrect;
             //Number sliders
             if (sliders[id].type == "number") {
                 //Do not display angular velocity or rotation for point charges
                 if ((sliders[id].name == "angular_velocity" || sliders[id].name == "rotation") && this.curObj instanceof PointCharge) continue;
                 if (sliders[id].name == "mass" && this.curObj instanceof InfinitePlane) continue;
                 //Calculate corrected min and max values according to non-linear input
-                let min = ObjEditor.unCorrection(sliders[id].min as number, isNonLinear);
-                let max = ObjEditor.unCorrection(sliders[id].max as number, isNonLinear);
-                let val = ObjEditor.unCorrection(this.curState[i] as number, isNonLinear);
+                let min = uncorrect(sliders[id].min as number);
+                let max = uncorrect(sliders[id].max as number);
+                let val = uncorrect(this.curState[i] as number);
                 html += `
                     <div class="input_slider_range">
                         <input id="slider_range${i}" type="range" min="${min}" max="${max}" value="${val}" step="${sliders[id].step}" oninput="scene.objEditor.input('${i}',this.value)"/></div>
@@ -139,18 +165,12 @@ export default class ObjEditor {
             //Vector inputs
             else if (sliders[id].type == "vector") {
                 let min = sliders[id].min as Vector;
-                let minx = ObjEditor.unCorrection(min.x, isNonLinear);
-                let miny = ObjEditor.unCorrection(min.y, isNonLinear);
                 let max = sliders[id].max as Vector;
-                let maxx = ObjEditor.unCorrection(max.x, isNonLinear);
-                let maxy = ObjEditor.unCorrection(max.y, isNonLinear);
                 let val = this.curState[i] as Vector;
-                let valx = ObjEditor.unCorrection(val.x, isNonLinear);
-                let valy = ObjEditor.unCorrection(val.y, isNonLinear);
                 html += `
                     <div class="input_slider_vector">
-                        <input id="range_x${i}" type="range" min="${minx}" max="${maxx}" step="${sliders[id].step}" value="${valx}" oninput="scene.objEditor.input('${i}',this.value,'x')"/>
-                        <input id="range_y${i}" type="range" min="${miny}" max="${maxy}" step="${sliders[id].step}" value="${valy}" oninput="scene.objEditor.input('${i}',this.value,'y')"/>
+                        <input id="range_x${i}" type="range" min="${uncorrect(min.x)}" max="${uncorrect(max.x)}" step="${sliders[id].step}" value="${uncorrect(val.x)}" oninput="scene.objEditor.input('${i}',this.value,'x')"/>
+                        <input id="range_y${i}" type="range" min="${uncorrect(min.y)}" max="${uncorrect(max.y)}" step="${sliders[id].step}" value="${uncorrect(val.y)}" oninput="scene.objEditor.input('${i}',this.value,'y')"/>
                     </div>
                 `;
             }
@@ -168,7 +188,8 @@ export default class ObjEditor {
     }
     input = (name: string, value: number | string, direction?: "x" | "y") => {
         if (!this.curObj) return;
-        value = ObjEditor.correction(Number(value), sliders[sliders.findIndex(v => v.name == name)].nonLinear);
+        let slider = sliders[getSliderId(name, this.curType)];
+        value = slider.correction.correct(Number(value));
         value = Math.round(value * 100) / 100;
         if (name == "position") {
             if (direction == "x") this.curObj.position.x = value;
@@ -202,19 +223,20 @@ export default class ObjEditor {
         this.updateDisplay(name, value, false);
     }
     updateDisplay = (name: string, value: number | Vector, setInput: boolean = true) => {
-        let isNonLinear = sliders[sliders.findIndex(v => v.name == name)].nonLinear;
+        let slider = sliders[getSliderId(name, this.curType)]
+        let uncorrect = slider.correction.uncorrect;
         //Update interface display
         if (value instanceof Vector) {
             document.querySelector("#slider_value" + name).innerHTML = value.toString();
-            let valx = ObjEditor.unCorrection(value.x, isNonLinear);
-            let valy = ObjEditor.unCorrection(value.y, isNonLinear);
+            let valx = uncorrect(value.x);
+            let valy = uncorrect(value.y);
             if (setInput) document.querySelector<HTMLInputElement>("#range_x" + name).value = valx.toString();
             if (setInput) document.querySelector<HTMLInputElement>("#range_y" + name).value = valy.toString();
         }
         else if (typeof value == "number") {
             let roundedValue = Math.round(value * 100) / 100;
             document.querySelector("#slider_value" + name).innerHTML = roundedValue.toString();
-            value = ObjEditor.unCorrection(value, isNonLinear);
+            value = uncorrect(value);
             if (setInput) document.querySelector<HTMLInputElement>("#slider_range" + name).value = value.toString();
         }
     }
