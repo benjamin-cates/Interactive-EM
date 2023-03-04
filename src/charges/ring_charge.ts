3d6909435289720512ce398f318ded1bd7434e9e
Joey Battistoni <56656056+joejoebat@users.noreply.github.com>
2023-03-03 17:08:15 -0800

    Create ring_charge.ts

---
+++ src/charges/ring_charge.ts
@@ -0,0 +1,12 @@
+import { Object, ObjectTypes } from "../base";
+import Vector from "../vector";
+import Scene from "../scene";
+import constants from "../constants";
+
+export default class RingCharge extends Object {
+    //in microcolumbs
+    charge: number;
+    radius: number;
+
+    
+}
