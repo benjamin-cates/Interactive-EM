8bd57b98f647891008685cb089371cc223cec51c
Benjamin Cates <thebenjicat@gmail.com>
2023-03-08 10:00:49 -0800

    Changes on composition object

--- src/charges/composition.ts
+++ src/charges/composition.ts
@@ -5,6 +5,8 @@ export default class Composition extends Object {
 
     //All coordinate systems of the elements in objs are relative to the center of mass and default rotation of the composition
     objs: Object[];
+    relPos: Vector[];
+    relRot: number[];
 
     constructor(objs: Object[], position: Vector, rotation: number = 0) {
         //Calculate total mass and translate all objects relative to the center of mass
@@ -14,28 +16,55 @@ export default class Composition extends Object {
             mass += obj.mass;
             COM.add(Vector.multiply(obj.position, obj.mass));
         });
-        let negativeCOM = Vector.multiply(COM, -1 / mass);
-        objs.forEach(obj => {
-            obj.position.add(negativeCOM);
-        });
         super(mass, position, rotation);
+        this.relPos = [];
+        this.relRot = [];
+        objs.forEach((obj, i) => {
+            this.relPos[i] = Vector.subtract(obj.position, COM);
+            this.relRot[i] = obj.rotation;
+        });
         this.objs = objs;
     }
 
     sumForces = (forces: { force: Vector, torque: number }[]): { force: Vector, torque: number } => {
-        //TODO: calculate total force and torque based on the force and torque of each object
+        //Torque is the sum of torques plus the sum of the cross products of the position vectors and the forces
+        //Force is just the sum of forces
 
-        return { force: Vector.origin(), torque: 0 };
+        let netForce = Vector.origin();
+        let netTorque = 0;
+        for (let i = 0; i < forces.length; i++) {
+            netForce.add(forces[i].force);
+            netTorque += forces[i].torque;
+            netTorque += Vector.cross2D(this.relPos[i], forces[i].force);
+        }
+        return { force: netForce, torque: netTorque };
+    }
+    updatePosition = () => {
+        let rotmat = Vector.rotationMatrix(this.rotation);
+        for (let i = 0; i < this.objs.length; i++) {
+            this.objs[i].position = Vector.add(this.position, Vector.transform(this.relPos[i], rotmat));
+            this.objs[i].updatePosition();
+            this.objs[i].rotation = this.rotation + this.relRot[i];
+        }
+    }
+    updateRotation = () => {
+        this.updatePosition();
     }
 
     render = (ctx: CanvasRenderingContext2D) => {
-        ctx.save();
-        ctx.translate(this.position.x * 100, this.position.y * 100);
-        ctx.rotate(this.rotation);
-        this.objs.forEach(obj => {
-            obj.render(ctx);
-        });
-        ctx.restore();
+        this.objs.forEach(obj => obj.render(ctx));
     }
+
+    fieldAt = (pos: Vector): Vector => {
+        let field = Vector.origin();
+        this.objs.forEach(obj => field.add(obj.fieldAt(pos)));
+        return field;
+    }
+    voltageAt = (pos: Vector): number => {
+        let volt = 0;
+        this.objs.forEach(obj => volt += obj.voltageAt(pos));
+        return volt;
+    }
+
 
 }
