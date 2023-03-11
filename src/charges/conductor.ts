9719dbdcd2e1d2020f2e30958d5b807c8cd057ae
Benjamin Cates <thebenjicat@gmail.com>
2023-03-10 23:40:00 -0800

    Conductor base class members

--- src/charges/conductor.ts
+++ src/charges/conductor.ts
@@ -1,7 +1,15 @@
-import {Object} from "../base";
+import { Object } from "../base";
+import Vector from "../vector";
+import PointCharge from "./point_charge";
+import Scene from "../scene";
 
 export default class Conductor extends Object {
-    width: number;
-    height: number;
+    //LAYOUT
+    points: Vector[];
+    normals: Vector[];
+    //STATE
+    chargeDensities: number[];
+    netCharge: number;
+
 
 }
