2657252d22acd54a70fae0970af273fc0b1d022f
Juicysm00th <60585843+Juicysm00th@users.noreply.github.com>
2023-03-24 17:53:58 -0700

    Update ring_charge.ts

--- src/charges/ring_charge.ts
+++ src/charges/ring_charge.ts
@@ -7,6 +7,12 @@ export default class RingCharge extends Object {
     //in microcolumbs
     charge: number;
     radius: number;
-
+    fieldAt = (pos: Vector) => {
+        return Vector.multiply(Vector.inverseSquareField(pos, this.position), constants.K * this.charge);
+    }
+    voltageAt: (pos: Vector) =>{
+        let distance: number = Vector.distance(pos, this.position)
+        return ((constants.K * this.charge)/ distance);
+    }
     
 }
